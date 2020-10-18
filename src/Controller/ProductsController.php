74691526cfe62a787b06fbf74fac0cb52756f545
hyngwar <seokkjj83@gmail.com>
2020-10-18 14:54:08 +0200

    no message

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ProductsController extends AbstractController
 {
     /**
      * @Route("/products", name="products")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
+        $usuario = $session->get('usuario');
         return $this->render('products.html.twig', [
             'controller_name' => 'ProductsController',
+            'usuario' => strlen($usuario)>0?'Hola '.$usuario:'',
         ]);
     }
 }
