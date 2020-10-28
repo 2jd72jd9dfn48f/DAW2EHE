cf0d944772d8ddacf141aa6f23dd01762bdc730b
hyngwar <seokkjj83@gmail.com>
2020-10-28 09:37:26 +0100

    no message

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,19 +4,55 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ProductsController extends AbstractController
 {
     /**
-     * @Route("/products", name="products")
+     * @Route("/products/{products}", name="products")
      */
-    public function index(SessionInterface $session)
+    public function index($products=1,Request $request, SessionInterface $session)
     {
         $usuario = $session->get('usuario');
-        return $this->render('products.html.twig', [
-            'controller_name' => 'ProductsController',
-            'usuario' => strlen($usuario)>0?'Hola '.$usuario:'',
-        ]);
-    }
+        $userlogin = strlen($usuario)>0?'Hola '.$usuario:'';
+
+        $enlace = $this->generateUrl('products',[
+            'products'=> 1,
+            ]);
+        $enlace2 = $this->generateUrl('products',[
+            'products'=> 2,
+            ]);
+        $enlace3 = $this->generateUrl('products',[
+            'products'=> 3,
+            ]);
+
+        switch($products){
+            case "2":
+                return $this->render('products2.html.twig', [
+                    'enlace' => $enlace,
+                    'enlace2' => $enlace2,
+                    'enlace3' => $enlace3,
+                    'usuario' => $userlogin,
+                ]);
+                break;
+            case "3":
+                return $this->render('products3.html.twig', [
+                    'enlace' => $enlace,
+                    'enlace2' => $enlace2,
+                    'enlace3' => $enlace3,
+                    'usuario' => $userlogin,
+                ]);
+                break;
+            default:
+                return $this->render('products.html.twig', [
+                    'enlace' => $enlace,
+                    'enlace2' => $enlace2,
+                    'enlace3' => $enlace3,
+                    'usuario' => $userlogin,
+                ]);
+                break;
+        }
+
+    } 
 }
