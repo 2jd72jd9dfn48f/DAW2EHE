46160045a5b3c7ae21bc9cff8b4eba0414c12538
hyngwar <seokkjj83@gmail.com>
2021-01-03 21:52:52 +0100

    no message

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\{Tour,Categoria,Contacto};
+use App\Form\ContactoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -13,10 +15,14 @@ class ContactController extends AbstractController
      */
     public function index(SessionInterface $session)
     {
+        $contacto=new Contacto();
+        $form= $this->createForm(ContactoType::class, $contacto);
+
         $usuario = $session->get('usuario');
         return $this->render('contact.html.twig', [
             'controller_name' => 'ContactController',
             'usuario' => strlen($usuario)>0?'Hola '.$usuario:'',
+            'form'=>$form->createView()
         ]);
     }
 
